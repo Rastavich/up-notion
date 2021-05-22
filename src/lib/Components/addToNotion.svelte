4b286d708b55ba0745306214c0c5c952a5acc722
Rastavich <isaac.hauser@live.com.au>
2021-05-22 19:43:09 +1000

    Working on the API

--- src/lib/Components/addToNotion.svelte
+++ src/lib/Components/addToNotion.svelte
@@ -1,104 +1,97 @@
 <script>
-    import { onMount } from 'svelte';
-    import { trans } from '$lib/transactionStore.js';
-    import { createPage } from '$lib/requests.js';
-    import { NOTION_API, NOTION_DATABASE_ID } from '$lib/variables.js';
+import { onMount } from 'svelte';
+import { trans } from '$lib/transactionStore.js';
+import { createPage } from '$lib/requests.js';
+import { NOTION_API, NOTION_DATABASE_ID } from '$lib/variables.js';
 
-    import * as api from '$lib/api.js';
-    import {
-        respond
-    } from '$lib/respond.js';
+import * as api from '$lib/api.js';
+import { respond } from '$lib/respond.js';
 
-   async function getBody(item, index) {
-  
+async function getBody(item, index) {
+
     const data = JSON.stringify({
         parent: {
-          database_id: `${NOTION_DATABASE_ID}`
+            database_id: `${NOTION_DATABASE_ID}`
         },
         properties: {
-          Tags: {
-            id: 'FB|y',
-            type: 'select',
-            select: {
-              id: '6e86cb06-1464-4af3-9ba7-3a8afef386f3',
-              name: 'Monthly',
-              color: 'brown'
-            }
-          },
-          Company: {
-            id: 'NEcW',
-            type: 'select',
-            select: {
-              id: '3b433831-d710-47ad-963b-bed522e13f8d',
-              name: 'Description',
-              color: 'default'
-            }
-          },
-          Total: {
-            id: 'Qvu`',
-            type: 'formula',
-            formula: {
-              type: 'number',
-              number: null
-            }
-          },
-          Type: {
-            id: '[c{o',
-            type: 'select',
-            select: {
-              id: 'b892d96a-4509-4071-bc2b-69beab1ed15b',
-              name: 'Expense',
-              color: 'orange'
-            }
-          },
-          Amount: {
-            id: 'a@LY',
-            type: 'number',
-            number: parseInt($trans[index].attributes.amount.value)
-          },
-          Name: {
-            id: 'title',
-            type: 'title',
-            title: [
-              {
-                type: 'text',
-                text: {
-                  content: $trans[index].attributes.description,
-                  link: null
+            Tags: {
+                select: {
+                    id: "28aa9649-d41d-4753-b0d2-b45e4f3fca34",
+                    name: "Weekly",
+                    color: "default"
+                }
+            },
+            Date: {
+                id: "MGZE",
+                type: "date",
+                date: {
+                    start: $trans[index].attributes.createdAt.toString()
+                }
+            },
+            Company: {
+                id: 'NEcW',
+                type: 'select',
+                select: {
+                    id: "b7994623-b46e-425d-93cd-cea0d2368b9e",
+                    name: "Food",
+                    color: "yellow"
+                }
+            },
+            Type: {
+                id: '[c{o',
+                type: 'select',
+                select: {
+                    id: "e1661fc5-e5f8-444b-8667-a34c48e419f0",
+                    name: "Expense",
+                    color: "yellow"
                 },
-                annotations: {
-                  bold: false,
-                  italic: false,
-                  strikethrough: false,
-                  underline: false,
-                  code: false,
-                  color: 'default'
-                },
-                plain_text: $trans[index].attributes.description,
-                href: null
-              }
-            ]
-          }
+            },
+            Amount: {
+                id: 'a@LY',
+                type: 'number',
+                number: parseInt($trans[index].attributes.amount.value)
+            },
+            Name: {
+                id: 'title',
+                type: 'title',
+                title: [{
+                    type: 'text',
+                    text: {
+                        content: $trans[index].attributes.description,
+                        link: null
+                    },
+                    annotations: {
+                        bold: false,
+                        italic: false,
+                        strikethrough: false,
+                        underline: false,
+                        code: false,
+                        color: 'default'
+                    },
+                    plain_text: $trans[index].attributes.description,
+                    href: null
+                }]
+            }
         }
-      });
+    });
 
-      const url = 'https://thingproxy.freeboard.io/fetch/https://api.notion.com/v1/';
-      const body = await api.post(
-          data
-      , {token: NOTION_API, params: 'pages'},
-      url)
+    const url = 'https://thingproxy.freeboard.io/fetch/https://api.notion.com/v1/';
+    const body = await api.post(
+        data, {
+            token: NOTION_API,
+            params: 'pages'
+        },
+        url)
 
+    const db = await respond(body);
 
-      const db = await respond(body);
+}
 
-   }
-
-   function addToNotion() {
-     $trans.forEach(getBody);
-   }
-  
+function addToNotion() {
+    $trans.forEach(getBody);
+}
 </script>
 
 <section>
-   <button on:click={addToNotion}>Add transactions to Notion</button> 
+    <button on:click={addToNotion}>Add transactions to Notion</button>
 </section>
--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import vercel from '@sveltejs/adapter-vercel';
+import sveltePreprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -6,7 +7,9 @@ const config = {
 		// hydrate the <div id="svelte"> element in src/app.html
 		adapter: vercel(),
 		target: '#svelte'
-	}
+	},
+
+	preprocess: [sveltePreprocess()]
 };
 
 export default config;
