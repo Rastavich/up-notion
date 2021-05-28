13b593440b785ec3fc070357b2a9195a38d8a57a
Rastavich <isaac.hauser@live.com.au>
2021-05-28 18:18:02 +1000

    pagination

--- src/lib/Components/addToNotion.svelte
+++ src/lib/Components/addToNotion.svelte
@@ -1,97 +1,97 @@
 <script>
-import { onMount } from 'svelte';
-import { trans } from '$lib/transactionStore.js';
-import { createPage } from '$lib/requests.js';
-import { NOTION_API, NOTION_DATABASE_ID } from '$lib/variables.js';
+	import { trans } from '$lib/transactionStore.js';
+	import { NOTION_API, NOTION_DATABASE_ID } from '$lib/variables.js';
+	import * as api from '$lib/api.js';
+	import { respond } from '$lib/respond.js';
 
-import * as api from '$lib/api.js';
-import { respond } from '$lib/respond.js';
+	async function getBody(item, index) {
+		const data = JSON.stringify({
+			parent: {
+				database_id: `${NOTION_DATABASE_ID}`
+			},
+			properties: {
+				Tags: {
+					select: {
+						id: '28aa9649-d41d-4753-b0d2-b45e4f3fca34',
+						name: 'Weekly',
+						color: 'default'
+					}
+				},
+				Date: {
+					id: 'MGZE',
+					type: 'date',
+					date: {
+						start: $trans[index].attributes.createdAt.toString()
+					}
+				},
+				Company: {
+					id: 'NEcW',
+					type: 'select',
+					select: {
+						id: 'b7994623-b46e-425d-93cd-cea0d2368b9e',
+						name: 'Food',
+						color: 'yellow'
+					}
+				},
+				Type: {
+					id: '[c{o',
+					type: 'select',
+					select: {
+						id: 'e1661fc5-e5f8-444b-8667-a34c48e419f0',
+						name: 'Expense',
+						color: 'yellow'
+					}
+				},
+				Amount: {
+					id: 'a@LY',
+					type: 'number',
+					number: parseInt($trans[index].attributes.amount.value)
+				},
+				Name: {
+					id: 'title',
+					type: 'title',
+					title: [
+						{
+							type: 'text',
+							text: {
+								content: $trans[index].attributes.description,
+								link: null
+							},
+							annotations: {
+								bold: false,
+								italic: false,
+								strikethrough: false,
+								underline: false,
+								code: false,
+								color: 'default'
+							},
+							plain_text: $trans[index].attributes.description,
+							href: null
+						}
+					]
+				}
+			}
+		});
 
-async function getBody(item, index) {
+		const url = 'https://thingproxy.freeboard.io/fetch/https://api.notion.com/v1/';
+		const body = await api.post(
+			data,
+			{
+				token: NOTION_API,
+				params: 'pages'
+			},
+			url
+		);
 
-    const data = JSON.stringify({
-        parent: {
-            database_id: `${NOTION_DATABASE_ID}`
-        },
-        properties: {
-            Tags: {
-                select: {
-                    id: "28aa9649-d41d-4753-b0d2-b45e4f3fca34",
-                    name: "Weekly",
-                    color: "default"
-                }
-            },
-            Date: {
-                id: "MGZE",
-                type: "date",
-                date: {
-                    start: $trans[index].attributes.createdAt.toString()
-                }
-            },
-            Company: {
-                id: 'NEcW',
-                type: 'select',
-                select: {
-                    id: "b7994623-b46e-425d-93cd-cea0d2368b9e",
-                    name: "Food",
-                    color: "yellow"
-                }
-            },
-            Type: {
-                id: '[c{o',
-                type: 'select',
-                select: {
-                    id: "e1661fc5-e5f8-444b-8667-a34c48e419f0",
-                    name: "Expense",
-                    color: "yellow"
-                },
-            },
-            Amount: {
-                id: 'a@LY',
-                type: 'number',
-                number: parseInt($trans[index].attributes.amount.value)
-            },
-            Name: {
-                id: 'title',
-                type: 'title',
-                title: [{
-                    type: 'text',
-                    text: {
-                        content: $trans[index].attributes.description,
-                        link: null
-                    },
-                    annotations: {
-                        bold: false,
-                        italic: false,
-                        strikethrough: false,
-                        underline: false,
-                        code: false,
-                        color: 'default'
-                    },
-                    plain_text: $trans[index].attributes.description,
-                    href: null
-                }]
-            }
-        }
-    });
+		const db = await respond(body);
+	}
 
-    const url = 'https://thingproxy.freeboard.io/fetch/https://api.notion.com/v1/';
-    const body = await api.post(
-        data, {
-            token: NOTION_API,
-            params: 'pages'
-        },
-        url)
+	function addToNotion() {
+		$trans.forEach(getBody);
+	}
 
-    const db = await respond(body);
-
-}
-
-function addToNotion() {
-    $trans.forEach(getBody);
-}
 </script>
 
 <section>
-    <button on:click={addToNotion}>Add transactions to Notion</button>
+	<button on:click={addToNotion}>Add transactions to Notion</button>
 </section>
